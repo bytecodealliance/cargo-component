15017eed9583b3bd3e1a44358ded1a4454d8b2da
Peter Huene <phuene@fastly.com>
2022-05-01 22:04:23 -0700

    Add tests for the `clippy` command.
    This commit adds some basic tests for the `cargo component clippy` command.

--- tests/clippy.rs
+++ tests/clippy.rs
@@ -1,6 +1,8 @@
 use crate::support::*;
+use anyhow::Result;
 use assert_cmd::prelude::*;
-use predicates::str::contains;
+use predicates::{boolean::PredicateBooleanExt, str::contains};
+use std::{fmt::Write, fs};
 
 mod support;
 
@@ -14,4 +16,57 @@ fn help() {
             ))
             .success();
     }
+}
+
+#[test]
+fn it_checks_a_new_project() -> Result<()> {
+    let project = Project::new("foo")?;
+    project
+        .cargo_component("clippy")
+        .assert()
+        .stderr(contains("Checking interface v0.1.0"))
+        .success();
+
+    Ok(())
+}
+
+#[test]
+fn it_finds_errors() -> Result<()> {
+    let project = Project::new("foo")?;
+
+    let mut src = fs::read_to_string(project.root().join("src/lib.rs"))?;
+    write!(&mut src, "\n\nfn foo() -> String {{\n  \"foo\"\n}}\n")?;
+
+    fs::write(project.root().join("src/lib.rs"), src)?;
+
+    project
+        .cargo_component("clippy")
+        .assert()
+        .stderr(contains("Checking interface v0.1.0").and(contains(
+            "expected struct `std::string::String`, found `&str`",
+        )))
+        .failure();
+
+    Ok(())
+}
+
+#[test]
+fn it_finds_clippy_warnings() -> Result<()> {
+    let project = Project::new("foo")?;
+
+    let mut src = fs::read_to_string(project.root().join("src/lib.rs"))?;
+    write!(
+        &mut src,
+        "\n\nfn foo() -> String {{\n  return \"foo\".to_string();\n}}\n"
+    )?;
+
+    fs::write(project.root().join("src/lib.rs"), src)?;
+
+    project
+        .cargo_component("clippy")
+        .assert()
+        .stderr(contains("Checking interface v0.1.0").and(contains("clippy::needless_return")))
+        .success();
+
+    Ok(())
 }
