7e387e4945531203d107448309749f03c044b97a
Peter Huene <phuene@fastly.com>
2022-05-02 10:20:22 -0700

    Add more `help` command tests.
    This commit adds tests for each supported subcommand to the help tests.

--- tests/help.rs
+++ tests/help.rs
@@ -13,3 +13,59 @@ fn help() {
             .success();
     }
 }
+
+#[test]
+fn shows_add_help() {
+    cargo_component("help add")
+        .assert()
+        .stdout(contains("Add a dependency for a WebAssembly component"))
+        .success();
+}
+
+#[test]
+fn shows_build_help() {
+    cargo_component("help build")
+        .assert()
+        .stdout(contains(
+            "Compile a WebAssembly component and all of its dependencies",
+        ))
+        .success();
+}
+
+#[test]
+fn shows_check_help() {
+    cargo_component("help check")
+        .assert()
+        .stdout(contains(
+            "Check a local package and all of its dependencies for errors",
+        ))
+        .success();
+}
+
+#[test]
+fn shows_clippy_help() {
+    cargo_component("help clippy")
+        .assert()
+        .stdout(contains(
+            "Checks a package to catch common mistakes and improve your Rust code",
+        ))
+        .success();
+}
+
+#[test]
+fn shows_metadata_help() {
+    cargo_component("help metadata")
+        .assert()
+        .stdout(contains("Output the resolved dependencies of a package"))
+        .success();
+}
+
+#[test]
+fn shows_new_help() {
+    cargo_component("help new")
+        .assert()
+        .stdout(contains(
+            "Create a new WebAssembly component package at <path>",
+        ))
+        .success();
+}
