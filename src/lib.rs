ac70c26fe6f27450e4fceafb3d036e80a7eaae7c
Peter Huene <peter@huene.dev>
2023-03-20 11:53:42 -0700

    Update dependencies.
    This commit updates dependencies to latest.
    
    The WASI preview2 adapters have been updated to `408f0bf`.
    
    The update includes a pre-release patch of `wit-bindgen` to fix building of
    components that only have imports (i.e. an adapted command component that has
    an additional import specified).

--- src/lib.rs
+++ src/lib.rs
@@ -235,9 +235,9 @@ fn create_component(config: &Config, path: &Path, binary: bool) -> Result<()> {
         .adapter(
             "wasi_snapshot_preview1",
             if binary {
-                include_bytes!("../adapters/1f93e4e/wasi_snapshot_preview1.command.wasm")
+                include_bytes!("../adapters/408f0bf/wasi_snapshot_preview1.command.wasm")
             } else {
-                include_bytes!("../adapters/1f93e4e/wasi_snapshot_preview1.reactor.wasm")
+                include_bytes!("../adapters/408f0bf/wasi_snapshot_preview1.reactor.wasm")
             },
         )?
         .module(&module)?
