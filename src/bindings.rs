ce3fca6f4640e7dfa82516068951e897bc9cff72
Peter Huene <peter@huene.dev>
2023-06-09 20:15:52 -0700

    Implement use declarations for `--target` option.
    This commit implements generating `use` declarations for the `--target` option
    of the `new` command.
    
    Previously, all types from the bindings were using fully-qualified paths.
    
    This implementation uses a simplistic trie to generate `use` declarations of
    types from the bindings.
    
    Conflicting type names result in fully qualified paths for any types that
    conflict with existing `use` declarations.
    
    Also bumps the `wit-bindgen` version output from the `new` command to 0.7.0.
    
    Closes #89.

--- src/bindings.rs
+++ src/bindings.rs
@@ -6,12 +6,13 @@ use crate::{
     registry::{DecodedDependency, PackageDependencyResolution},
 };
 use anyhow::{bail, Context, Result};
-use heck::{ToSnakeCase, ToUpperCamelCase};
+use heck::{AsSnakeCase, ToSnakeCase, ToUpperCamelCase};
 use indexmap::{IndexMap, IndexSet};
 use semver::Version;
 use std::{
+    borrow::Cow,
     collections::{HashMap, HashSet},
-    fmt::Write,
+    fmt::{self, Write},
     fs,
     io::Read,
     path::{Path, PathBuf},
@@ -28,7 +29,7 @@ use wit_parser::{
 };
 
 pub(crate) const BINDINGS_VERSION: &str = "0.1.0";
-pub(crate) const WIT_BINDGEN_VERSION: &str = "0.6.0";
+pub(crate) const WIT_BINDGEN_VERSION: &str = "0.7.0";
 
 fn named_world_key<'a>(resolve: &'a Resolve, orig: &'a WorldKey, prefix: &str) -> WorldKey {
     let name = match orig {
@@ -643,32 +644,173 @@ publish = false
     }
 }
 
-fn export_world_key_path(resolve: &Resolve, key: &WorldKey) -> String {
-    match key {
-        WorldKey::Name(name) => format!("bindings::exports::{name}", name = name.to_snake_case(),),
-        WorldKey::Interface(id) => {
-            let iface = &resolve.interfaces[*id];
-            let pkg = &resolve.packages[iface.package.expect("interface should have a package")];
-            let name = iface.name.as_deref().expect("unnamed interface");
-            format!(
-                "bindings::exports::{ns}::{pkg}::{name}",
-                ns = pkg.name.namespace.to_snake_case(),
-                pkg = pkg.name.name.to_snake_case(),
-                name = name.to_snake_case(),
+/// Represents a node in a "use" trie.
+#[derive(Default)]
+struct UseTrieNode {
+    // Map of segment index to trie node
+    children: HashMap<usize, UseTrieNode>,
+    // Set of type indexes that are used at this node
+    tys: HashSet<usize>,
+}
+
+impl UseTrieNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>, trie: &UseTrie) -> fmt::Result {
+        // Print the children first
+        for (i, (segment, child)) in self.children.iter().enumerate() {
+            if i > 0 {
+                write!(f, ", ")?;
+            }
+
+            write!(f, "{segment}::{{", segment = trie.segments[*segment])?;
+            child.fmt(f, trie)?;
+            write!(f, "}}")?;
+        }
+
+        if !self.children.is_empty() && !self.tys.is_empty() {
+            write!(f, ", ")?;
+        }
+
+        // Next, print the types at this node
+        for (i, type_index) in self.tys.iter().enumerate() {
+            if i > 0 {
+                write!(f, ", ")?;
+            }
+
+            write!(f, "{ty}", ty = trie.types[*type_index])?;
+        }
+
+        Ok(())
+    }
+}
+
+/// A trie that maintains uses of types when generating Rust source code.
+#[derive(Default)]
+struct UseTrie {
+    /// The root trie node.
+    root: UseTrieNode,
+    /// The set of path segments.
+    segments: IndexSet<String>,
+    /// The set of type names.
+    types: IndexSet<String>,
+}
+
+impl UseTrie {
+    /// Gets the used types at a given path.
+    fn get<'a>(&self, path: impl Iterator<Item = &'a str>) -> Option<impl Iterator<Item = &str>> {
+        let mut node = &self.root;
+        for segment in path {
+            let (index, _) = self.segments.get_full(segment)?;
+            node = node.children.get(&index)?;
+        }
+
+        Some(node.tys.iter().map(|i| self.types[*i].as_str()))
+    }
+
+    /// Inserts a new use of the given type.
+    ///
+    /// This method handles the proper casing for segment paths and type names.
+    ///
+    /// Returns the string to use when printing the type reference.
+    fn insert<'a, I>(&mut self, path: I, ty: &str) -> Cow<str>
+    where
+        I: IntoIterator<Item = &'a str>,
+        I::IntoIter: Clone,
+    {
+        let (type_index, inserted) = self.types.insert_full(ty.to_upper_camel_case());
+        if !inserted {
+            let path = path.into_iter();
+            let ty: &String = &self.types[type_index];
+
+            // Check to see if the type is already used at this path
+            if let Some(tys) = self.get(path.clone()) {
+                for existing in tys {
+                    if ty == existing {
+                        // Same path, so just return the type name
+                        return ty.into();
+                    }
+                }
+            }
+
+            // Type conflicts with an existing type, so use the qualified type name
+            return format!(
+                "{path}::{ty}",
+                path = path.enumerate().fold(String::new(), |mut s, (i, p)| {
+                    if i > 0 {
+                        s.push_str("::");
+                    }
+                    write!(s, "{p}", p = AsSnakeCase(p)).unwrap();
+                    s
+                }),
+                ty = self.types[type_index],
             )
+            .into();
+        }
+
+        let mut node = &mut self.root;
+        for segment in path {
+            assert!(!segment.is_empty());
+            let (segment_index, _) = self.segments.insert_full(segment.to_snake_case());
+            node = node.children.entry(segment_index).or_default();
+        }
+
+        node.tys.insert(type_index);
+
+        // Return just the type name as we were able to use this type unqualified
+        Cow::Borrowed(&self.types[type_index])
+    }
+
+    /// Inserts a type from a WIT interface.
+    fn insert_interface_type(
+        &mut self,
+        resolve: &Resolve,
+        interface: &Interface,
+        ty: &str,
+    ) -> Cow<str> {
+        let pkg = &resolve.packages[interface.package.expect("interface should have a package")];
+        let name = interface.name.as_deref().expect("unnamed interface");
+
+        self.insert(
+            [
+                "bindings",
+                "exports",
+                pkg.name.namespace.as_str(),
+                pkg.name.name.as_str(),
+                name,
+            ],
+            ty,
+        )
+    }
+
+    /// Inserts an export trait for the given world key.
+    fn insert_export_trait(&mut self, resolve: &Resolve, key: &WorldKey) -> Cow<str> {
+        match key {
+            WorldKey::Name(name) => self.insert(["bindings", "exports", name.as_str()], name),
+            WorldKey::Interface(id) => {
+                let iface = &resolve.interfaces[*id];
+                self.insert_interface_type(
+                    resolve,
+                    iface,
+                    iface.name.as_deref().expect("unnamed interface"),
+                )
+            }
         }
     }
 }
 
-fn export_trait_path(resolve: &Resolve, key: &WorldKey) -> String {
-    let path = export_world_key_path(resolve, key);
-    match key {
-        WorldKey::Name(name) => format!("{path}::{name}", name = name.to_upper_camel_case()),
-        WorldKey::Interface(id) => {
-            let iface = &resolve.interfaces[*id];
-            let name = iface.name.as_deref().expect("unnamed interface");
-            format!("{path}::{camel}", camel = name.to_upper_camel_case())
+impl fmt::Display for UseTrie {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        assert!(self.root.tys.is_empty());
+        for (segment_index, child) in &self.root.children {
+            write!(
+                f,
+                "use {segment}::{{",
+                segment = self.segments[*segment_index]
+            )?;
+            child.fmt(f, self)?;
+            writeln!(f, "}};")?;
         }
+
+        Ok(())
     }
 }
 
@@ -694,13 +836,9 @@ impl<'a> SourceGenerator<'a> {
     /// Generates the Rust source code for the given world.
     pub fn generate(&self, world: Option<&str>) -> Result<String> {
         let (resolve, world) = self.decode(world)?;
-        let mut source = String::new();
+        let mut trie = UseTrie::default();
+        let mut impls = Vec::new();
         let world = &resolve.worlds[world];
-
-        source.push_str("struct Component;\n");
-        if world.exports.is_empty() {
-            return Ok(source);
-        }
 
         let mut export_count = 0;
         let mut function_exports = Vec::new();
@@ -711,44 +849,66 @@ impl<'a> SourceGenerator<'a> {
                 }
                 WorldItem::Interface(i) => {
                     let interface = &resolve.interfaces[*i];
+                    let mut imp: String = String::new();
                     writeln!(
-                        &mut source,
-                        "\nimpl {path} for Component {{",
-                        path = export_trait_path(&resolve, key),
+                        &mut imp,
+                        "\nimpl {name} for Component {{",
+                        name = trie.insert_export_trait(&resolve, key),
                     )
                     .unwrap();
 
                     for (i, (_, func)) in interface.functions.iter().enumerate() {
                         if i > 0 {
-                            source.push('\n');
+                            imp.push('\n');
                         }
-                        Self::print_unimplemented_func(&resolve, func, &mut source)?;
+                        Self::print_unimplemented_func(&resolve, func, &mut imp, &mut trie)?;
                         export_count += 1;
                     }
 
-                    source.push_str("}\n");
+                    imp.push_str("}\n");
+                    impls.push(imp);
                 }
                 WorldItem::Type(_) => continue,
             }
         }
 
         if !function_exports.is_empty() {
+            let mut imp = String::new();
+
             writeln!(
-                &mut source,
-                "\nimpl bindings::{name} for Component {{",
-                name = world.name.to_upper_camel_case(),
+                &mut imp,
+                "\nimpl {name} for Component {{",
+                name = trie.insert(["bindings"], &world.name)
             )
             .unwrap();
 
             for (i, func) in function_exports.iter().enumerate() {
                 if i > 0 {
-                    source.push('\n');
+                    imp.push('\n');
                 }
-                Self::print_unimplemented_func(&resolve, func, &mut source)?;
+                Self::print_unimplemented_func(&resolve, func, &mut imp, &mut trie)?;
                 export_count += 1;
             }
 
-            source.push_str("}\n");
+            imp.push_str("}\n");
+            impls.push(imp);
+        }
+
+        let mut source = String::new();
+        write!(&mut source, "{trie}")?;
+
+        if !source.is_empty() {
+            source.push('\n');
+        }
+
+        source.push_str("struct Component;\n");
+
+        for (i, imp) in impls.iter().enumerate() {
+            if i > 0 {
+                source.push_str("\n\n");
+            }
+
+            source.push_str(imp);
         }
 
         if export_count > 0 {
@@ -815,6 +975,7 @@ impl<'a> SourceGenerator<'a> {
         resolve: &Resolve,
         func: &Function,
         source: &mut String,
+        trie: &mut UseTrie,
     ) -> Result<()> {
         // TODO: it would be nice to share the printing of the signature of the function
         // with wit-bindgen, but right now it's tightly coupled with interface generation.
@@ -825,14 +986,19 @@ impl<'a> SourceGenerator<'a> {
             }
             source.push_str(&to_rust_ident(name));
             source.push_str(": ");
-            Self::print_type(resolve, param, source)?;
+            Self::print_type(resolve, param, source, trie)?;
         }
         source.push(')');
         match func.results.len() {
             0 => {}
             1 => {
                 source.push_str(" -> ");
-                Self::print_type(resolve, func.results.iter_types().next().unwrap(), source)?;
+                Self::print_type(
+                    resolve,
+                    func.results.iter_types().next().unwrap(),
+                    source,
+                    trie,
+                )?;
             }
             _ => {
                 source.push_str(" -> (");
@@ -840,7 +1006,7 @@ impl<'a> SourceGenerator<'a> {
                     if i > 0 {
                         source.push_str(", ");
                     }
-                    Self::print_type(resolve, ty, source)?;
+                    Self::print_type(resolve, ty, source, trie)?;
                 }
                 source.push(')');
             }
@@ -849,7 +1015,12 @@ impl<'a> SourceGenerator<'a> {
         Ok(())
     }
 
-    fn print_type(resolve: &Resolve, ty: &Type, source: &mut String) -> Result<()> {
+    fn print_type(
+        resolve: &Resolve,
+        ty: &Type,
+        source: &mut String,
+        trie: &mut UseTrie,
+    ) -> Result<()> {
         match ty {
             Type::Bool => source.push_str("bool"),
             Type::U8 => source.push_str("u8"),
@@ -864,36 +1035,41 @@ impl<'a> SourceGenerator<'a> {
             Type::Float64 => source.push_str("f64"),
             Type::Char => source.push_str("char"),
             Type::String => source.push_str("String"),
-            Type::Id(id) => Self::print_type_id(resolve, *id, source)?,
+            Type::Id(id) => Self::print_type_id(resolve, *id, source, trie)?,
         }
 
         Ok(())
     }
 
-    fn print_type_id(resolve: &Resolve, id: TypeId, source: &mut String) -> Result<()> {
+    fn print_type_id(
+        resolve: &Resolve,
+        id: TypeId,
+        source: &mut String,
+        trie: &mut UseTrie,
+    ) -> Result<()> {
         let ty = &resolve.types[id];
 
         if ty.name.is_some() {
-            Self::print_type_path(resolve, ty, source);
+            Self::print_type_path(resolve, ty, source, trie);
             return Ok(());
         }
 
         match &ty.kind {
             TypeDefKind::List(ty) => {
                 source.push_str("Vec<");
-                Self::print_type(resolve, ty, source)?;
+                Self::print_type(resolve, ty, source, trie)?;
                 source.push('>');
             }
             TypeDefKind::Option(ty) => {
                 source.push_str("Option<");
-                Self::print_type(resolve, ty, source)?;
+                Self::print_type(resolve, ty, source, trie)?;
                 source.push('>');
             }
             TypeDefKind::Result(r) => {
                 source.push_str("Result<");
-                Self::print_optional_type(resolve, r.ok.as_ref(), source)?;
+                Self::print_optional_type(resolve, r.ok.as_ref(), source, trie)?;
                 source.push_str(", ");
-                Self::print_optional_type(resolve, r.err.as_ref(), source)?;
+                Self::print_optional_type(resolve, r.err.as_ref(), source, trie)?;
                 source.push('>');
             }
             TypeDefKind::Variant(_) => {
@@ -905,7 +1081,7 @@ impl<'a> SourceGenerator<'a> {
                     if i > 0 {
                         source.push_str(", ");
                     }
-                    Self::print_type(resolve, ty, source)?;
+                    Self::print_type(resolve, ty, source, trie)?;
                 }
                 source.push(')');
             }
@@ -923,48 +1099,54 @@ impl<'a> SourceGenerator<'a> {
             }
             TypeDefKind::Future(ty) => {
                 source.push_str("Future<");
-                Self::print_optional_type(resolve, ty.as_ref(), source)?;
+                Self::print_optional_type(resolve, ty.as_ref(), source, trie)?;
                 source.push('>');
             }
             TypeDefKind::Stream(stream) => {
                 source.push_str("Stream<");
-                Self::print_optional_type(resolve, stream.element.as_ref(), source)?;
+                Self::print_optional_type(resolve, stream.element.as_ref(), source, trie)?;
                 source.push_str(", ");
-                Self::print_optional_type(resolve, stream.end.as_ref(), source)?;
+                Self::print_optional_type(resolve, stream.end.as_ref(), source, trie)?;
                 source.push('>');
             }
-            TypeDefKind::Type(ty) => Self::print_type(resolve, ty, source)?,
+            TypeDefKind::Type(ty) => Self::print_type(resolve, ty, source, trie)?,
             TypeDefKind::Unknown => unreachable!(),
         }
 
         Ok(())
     }
 
-    fn print_type_path(resolve: &Resolve, ty: &TypeDef, source: &mut String) {
-        let name = ty.name.as_ref().unwrap().to_upper_camel_case();
-
+    fn print_type_path(resolve: &Resolve, ty: &TypeDef, source: &mut String, trie: &mut UseTrie) {
         if let TypeOwner::Interface(id) = ty.owner {
-            if resolve.interfaces[id].package.is_some() {
+            let interface = &resolve.interfaces[id];
+            if interface.package.is_some() {
                 write!(
                     source,
-                    "{path}::{name}",
-                    path = export_world_key_path(resolve, &WorldKey::Interface(id))
+                    "{name}",
+                    name =
+                        trie.insert_interface_type(resolve, interface, ty.name.as_deref().unwrap())
                 )
                 .unwrap();
                 return;
             }
         }
 
-        write!(source, "bindings::{name}").unwrap();
+        write!(
+            source,
+            "{name}",
+            name = trie.insert(["bindings"], ty.name.as_deref().unwrap())
+        )
+        .unwrap();
     }
 
     fn print_optional_type(
         resolve: &Resolve,
         ty: Option<&Type>,
         source: &mut String,
+        trie: &mut UseTrie,
     ) -> Result<()> {
         match ty {
-            Some(ty) => Self::print_type(resolve, ty, source)?,
+            Some(ty) => Self::print_type(resolve, ty, source, trie)?,
             None => source.push_str("()"),
         }
 
