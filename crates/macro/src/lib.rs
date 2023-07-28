dbb50b649b74e89b6b360e9756ea8e9149b2f856
Peter Huene <peter@huene.dev>
2023-07-27 17:03:41 -0700

    Implement support for resources.
    This commit implements support for resource types in the `generate!` macro.

--- crates/macro/src/lib.rs
+++ crates/macro/src/lib.rs
@@ -2,9 +2,13 @@
 
 #![deny(missing_docs)]
 
+use heck::ToUpperCamelCase;
 use proc_macro2::{Span, TokenStream};
 use quote::quote;
 use std::{
+    borrow::Cow,
+    collections::HashMap,
+    fmt::Write,
     fs,
     path::{Path, PathBuf},
 };
@@ -12,21 +16,55 @@ use syn::{
     parse::{Parse, ParseStream},
     parse_quote,
     punctuated::Punctuated,
-    spanned::Spanned,
-    token, Error, Result, Token, TypePath,
+    token, Error, Result, Token,
 };
 use wit_bindgen_core::{
-    wit_parser::{Resolve, WorldId},
+    wit_parser::{Resolve, TypeDefKind, WorldId, WorldItem, WorldKey},
     Files,
 };
-use wit_bindgen_rust::Opts;
+use wit_bindgen_rust::{ExportKey, Opts};
+use wit_bindgen_rust_lib::Ownership;
 use wit_component::DecodedWasm;
+
+fn implementor_path_str(path: &syn::Path) -> String {
+    let mut s = String::new();
+    s.push_str("super::");
+
+    for (i, segment) in path.segments.iter().enumerate() {
+        if i > 0 {
+            s.push_str("::");
+        }
+
+        write!(&mut s, "{ident}", ident = segment.ident).unwrap();
+    }
+
+    s
+}
 
 /// Used to generate bindings for a WebAssembly component.
 ///
+/// By default, all world exports are expected to be implemented
+/// on a type named `Component` where the `bindings!` macro
+/// is invoked.
+///
+/// Additionally, all resource exports are expected to be
+/// implemented on a type named `<ResourceName>`.
+///
+/// For example, a resource named `file` would be implemented
+/// on a type named `File` in the same scope as the `generate!`
+/// macro invocation.
+///
+/// # Options
+///
+/// The macro accepts the following options:
+///
+/// - `implementor`: The name of the type to implement world exports on.
+/// - `resources`: A map of resource names to resource implementor types.
+/// - `ownership`: The ownership model to use for resources.
+///
 /// # Examples
 ///
-/// Using the default implementor of `Component`:
+/// Using the default implementor names:
 ///
 /// ```ignore
 /// cargo_component_bindings::generate!()
@@ -36,9 +74,27 @@ use wit_component::DecodedWasm;
 ///
 /// ```ignore
 /// cargo_component_bindings::generate!({
-///    implementor: MyComponent,
+///     implementor: MyComponent,
 /// })
 /// ```
+///
+/// Specifying a custom resource implementor type named `MyResource`:
+///
+/// ```ignore
+/// cargo_component_bindings::generate!({
+///     resources: {
+///         "my:package/iface/res": MyResource,
+///     }
+/// })
+/// ```
+///
+/// Specifying the `borrowing-duplicate-if-necessary` ownership model
+/// for resources:
+///
+/// ```ignore
+/// cargo_component_bindings::generate!({
+///      ownership: borrowing-duplicate-if-necessary
+/// })
 #[proc_macro]
 pub fn generate(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     syn::parse_macro_input!(input as Config)
@@ -109,19 +165,61 @@ fn decode_resolve(path: &Path, span: Span) -> Result<(Resolve, WorldId)> {
 
 mod kw {
     syn::custom_keyword!(implementor);
+    syn::custom_keyword!(resources);
+    syn::custom_keyword!(ownership);
+}
+
+#[derive(Clone)]
+struct Resource {
+    key: syn::LitStr,
+    value: syn::Path,
+}
+
+impl Parse for Resource {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        let key = input.parse()?;
+        input.parse::<Token![:]>()?;
+        let value = input.parse()?;
+        Ok(Self { key, value })
+    }
 }
 
 enum Opt {
-    Implementor(TypePath),
+    Implementor(Span, syn::Path),
+    Resources(Span, Vec<Resource>),
+    Ownership(Span, Ownership),
 }
 
 impl Parse for Opt {
     fn parse(input: ParseStream<'_>) -> Result<Self> {
         let l = input.lookahead1();
         if l.peek(kw::implementor) {
-            input.parse::<kw::implementor>()?;
+            let span = input.parse::<kw::implementor>()?.span;
             input.parse::<Token![:]>()?;
-            Ok(Opt::Implementor(input.parse()?))
+            Ok(Opt::Implementor(span, input.parse()?))
+        } else if l.peek(kw::resources) {
+            let span = input.parse::<kw::resources>()?.span;
+            input.parse::<Token![:]>()?;
+            let contents;
+            syn::braced!(contents in input);
+            Ok(Opt::Resources(
+                span,
+                Punctuated::<_, Token![,]>::parse_terminated(&contents)?
+                    .iter()
+                    .cloned()
+                    .collect(),
+            ))
+        } else if l.peek(kw::ownership) {
+            let span = input.parse::<kw::ownership>()?.span;
+            input.parse::<Token![:]>()?;
+            let ownership = input.parse::<syn::Ident>()?;
+            Ok(Opt::Ownership(
+                span,
+                ownership
+                    .to_string()
+                    .parse()
+                    .map_err(|e| Error::new(ownership.span(), e))?,
+            ))
         } else {
             Err(l.error())
         }
@@ -132,22 +230,99 @@ struct Config {
     input: PathBuf,
     resolve: Resolve,
     world: WorldId,
-    implementor: Option<TypePath>,
+    implementor: Option<syn::Path>,
+    resources: HashMap<String, syn::Path>,
+    ownership: Ownership,
 }
 
 impl Config {
     fn expand(self) -> Result<TokenStream> {
-        let implementor = self.implementor.unwrap_or_else(|| parse_quote!(Component));
+        fn resource_implementor(
+            key: &str,
+            name: &str,
+            resources: &HashMap<String, syn::Path>,
+        ) -> String {
+            implementor_path_str(&resources.get(key).map(Cow::Borrowed).unwrap_or_else(|| {
+                Cow::Owned(
+                    syn::PathSegment::from(syn::Ident::new(
+                        &name.to_upper_camel_case(),
+                        Span::call_site(),
+                    ))
+                    .into(),
+                )
+            }))
+        }
+
+        let implementor =
+            implementor_path_str(&self.implementor.unwrap_or_else(|| parse_quote!(Component)));
+
+        let world = &self.resolve.worlds[self.world];
+        let mut exports = HashMap::new();
+        exports.insert(ExportKey::World, implementor.clone());
+
+        for (name, item) in &world.exports {
+            let key = match name {
+                WorldKey::Name(name) => name.clone(),
+                WorldKey::Interface(id) => {
+                    let interface = &self.resolve.interfaces[*id];
+                    let package = &self.resolve.packages
+                        [interface.package.expect("interface must have a package")];
+
+                    let mut key = String::new();
+                    key.push_str(&package.name.namespace);
+                    key.push(':');
+                    key.push_str(&package.name.name);
+                    key.push('/');
+                    key.push_str(interface.name.as_ref().expect("interface must have a name"));
+                    key
+                }
+            };
+
+            let implementor = match item {
+                WorldItem::Interface(id) => {
+                    let interface = &self.resolve.interfaces[*id];
+                    for (name, ty) in &interface.types {
+                        match self.resolve.types[*ty].kind {
+                            TypeDefKind::Resource => {
+                                let key = format!("{key}/{name}");
+                                let implementor = resource_implementor(&key, name, &self.resources);
+                                exports.insert(ExportKey::Name(key), implementor);
+                            }
+                            _ => continue,
+                        }
+                    }
+
+                    implementor.clone()
+                }
+                WorldItem::Type(id) => match self.resolve.types[*id].kind {
+                    TypeDefKind::Resource => resource_implementor(&key, &key, &self.resources),
+                    _ => continue,
+                },
+                WorldItem::Function(_) => implementor.clone(),
+            };
+
+            exports.insert(ExportKey::Name(key), implementor);
+        }
 
         let opts = Opts {
-            macro_call_prefix: Some("crate::bindings::".to_string()),
-            export_macro_name: Some("export".to_string()),
+            exports,
+            ownership: self.ownership,
             runtime_path: Some("cargo_component_bindings::rt".to_string()),
             ..Default::default()
         };
 
         let mut files = Files::default();
-        opts.build().generate(&self.resolve, self.world, &mut files);
+        opts.build()
+            .generate(&self.resolve, self.world, &mut files)
+            .map_err(|e| {
+                Error::new(
+                    Span::call_site(),
+                    format!(
+                        "failed to generate bindings from `{path}`: {e}",
+                        path = self.input.display()
+                    ),
+                )
+            })?;
 
         let sources: Vec<_> = files
             .iter()
@@ -158,17 +333,6 @@ impl Config {
             "expected exactly one source file to be generated"
         );
 
-        let (use_export, export) = if self.resolve.worlds[self.world].exports.is_empty() {
-            (quote!(), quote!())
-        } else {
-            (
-                quote!(
-                    pub(crate) use export;
-                ),
-                quote!(crate::bindings::export!(#implementor);),
-            )
-        };
-
         let source = sources[0].parse::<TokenStream>()?;
         let input = self.input.display().to_string();
 
@@ -176,20 +340,17 @@ impl Config {
             pub(crate) mod bindings {
                 #source
 
-                #use_export
-
                 const _: &[u8] = include_bytes!(#input);
             }
-
-            #export
         })
     }
 }
 
 impl Parse for Config {
     fn parse(input: ParseStream<'_>) -> Result<Self> {
-        let call_site = Span::call_site();
-        let mut implementor = None;
+        let mut implementor: Option<syn::Path> = None;
+        let mut resources: Option<Vec<Resource>> = None;
+        let mut ownership: Option<Ownership> = None;
 
         if input.peek(token::Brace) {
             let content;
@@ -197,28 +358,61 @@ impl Parse for Config {
             let options = Punctuated::<Opt, Token![,]>::parse_terminated(&content)?;
             for option in options.into_pairs() {
                 match option.into_value() {
-                    Opt::Implementor(path) => {
+                    Opt::Implementor(span, value) => {
                         if implementor.is_some() {
                             return Err(Error::new(
-                                path.span(),
+                                span,
                                 "cannot specify `implementor` more than once",
                             ));
                         }
 
-                        implementor = Some(path);
+                        if let Some(segment) = value.segments.first() {
+                            if segment.ident == "self" || segment.ident == "crate" {
+                                return Err(Error::new(
+                                    segment.ident.span(),
+                                    "cannot use `self` or `crate` as the implementor path",
+                                ));
+                            }
+                        }
+
+                        implementor = Some(value);
+                    }
+                    Opt::Resources(span, value) => {
+                        if resources.is_some() {
+                            return Err(Error::new(
+                                span,
+                                "cannot specify `resources` more than once",
+                            ));
+                        }
+
+                        resources = Some(value);
+                    }
+                    Opt::Ownership(span, value) => {
+                        if ownership.is_some() {
+                            return Err(Error::new(
+                                span,
+                                "cannot specify `ownership` more than once",
+                            ));
+                        }
+
+                        ownership = Some(value);
                     }
                 }
             }
         }
 
         let input = target_path()?;
-        let (resolve, world) = decode_resolve(&input, call_site)?;
+        let (resolve, world) = decode_resolve(&input, Span::call_site())?;
 
         Ok(Config {
             input,
             resolve,
             world,
             implementor,
+            resources: resources
+                .map(|r| r.into_iter().map(|r| (r.key.value(), r.value)).collect())
+                .unwrap_or_default(),
+            ownership: ownership.unwrap_or_default(),
         })
     }
 }
