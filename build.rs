3cc42eb5ad58eb1d271bed093c6fc9445a326099
Peter Huene <phuene@fastly.com>
2023-11-21 11:22:24 -0800

    Bump version to 0.5.0 (#195)
    * Update dependencies to latest.
    
    This includes new WASI adapters to sync with Wasmtime 15.0.0.
    
    * Bump version to 0.5.0.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, process::Command};
 
-const WASI_ADAPTER_VERSION: &str = "2da78ca";
+const WASI_ADAPTER_VERSION: &str = "6f0da84";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
