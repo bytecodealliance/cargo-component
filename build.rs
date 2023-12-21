a7fe522bffb82efa9aa3d126a1f9015c561f8416
Peter Huene <peter@huene.dev>
2023-12-21 17:25:38 -0500

    Update deps to latest.
    This also sync the WASI adapters to Wasmtime 16 release.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, process::Command};
 
-const WASI_ADAPTER_VERSION: &str = "6f0da84";
+const WASI_ADAPTER_VERSION: &str = "6613acd";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
